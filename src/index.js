8d113c80d6be02ee92db01e332c3c7498584c25c
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-02-13 12:42:03 -0500

    Added axial tilt, better comments

--- src/index.js
+++ src/index.js
@@ -3,29 +3,31 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import colorMap from "../textures/marscolormap.jpg";
 import bumpMap from "../textures/marsbumpmap.jpg";
 
-//initialize scene, camera, renderer
 const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight;
 
+//initialize renderer
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(canvasWidth, canvasHeight);
 document.body.appendChild(renderer.domElement);
 
+//initialize scene
 const scene = new THREE.Scene();
 
+//initialize camera
 const camera = new THREE.PerspectiveCamera(
   50,
   canvasWidth / canvasHeight,
   0.1,
   1000
 );
+camera.position.set(0, 0, 30);
 
+//initialize OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.75;
 controls.enablePan = false;
-
-camera.position.set(0, 0, 30);
 controls.update();
 
 //initialize textures
@@ -33,25 +35,39 @@ const textureLoader = new THREE.TextureLoader();
 textureLoader.crossOrigin = "";
 const marsColorMap = textureLoader.load(colorMap);
 const marsBumpMap = textureLoader.load(bumpMap);
-// const spaceTexture = textureLoader.load("textures/space.jpg");
 
-//lighting;
-const dLight = new THREE.DirectionalLight(0xffffff, 1);
-dLight.position.set(1, 0, 2);
-scene.add(dLight);
+//lighting
+const sunlight = new THREE.DirectionalLight(0xffffff, 1);
+sunlight.position.set(1, 0, 2);
+scene.add(sunlight);
 
-//create planet
-const geometry = new THREE.SphereGeometry(4, 30, 30);
-const material = new THREE.MeshStandardMaterial({
+//create mars
+const marsGeometry = new THREE.SphereGeometry(4, 30, 30);
+const marsMaterial = new THREE.MeshStandardMaterial({
   map: marsColorMap,
   bumpMap: marsBumpMap,
   bumpScale: 0.5,
 });
-const mars = new THREE.Mesh(geometry, material);
+const mars = new THREE.Mesh(marsGeometry, marsMaterial);
 scene.add(mars);
 
+//mars axial tilt
+mars.rotateZ(-0.43964844);
+const origin = new THREE.Vector3(0, 0, 0);
+const axialTilt = new THREE.Vector3(4.26, 9.05, 0);
+const points = [origin, axialTilt];
+
+// //create axis
+// const axisMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+// const axisGeometry = new THREE.BufferGeometry().setFromPoints(points);
+// const axisLine = new THREE.Line(axisGeometry, axisMaterial);
+// scene.add(axisLine);
+
+// //axis helper
+// const axesHelper = new THREE.AxesHelper(10);
+// scene.add(axesHelper);
+
 //create stars
-const starGeometry = new THREE.BufferGeometry();
 const starVertices = [];
 for (let i = 0; i < 10000; i++) {
   const x = (Math.random() - 0.5) * 2000 + 200;
@@ -60,22 +76,23 @@ for (let i = 0; i < 10000; i++) {
   starVertices.push(x, y, z);
 }
 const starVertices32 = new Float32Array(starVertices);
+
+const starGeometry = new THREE.BufferGeometry();
 starGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(starVertices32, 3)
 );
+
 const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+
 const stars = new THREE.Points(starGeometry, starMaterial);
 scene.add(stars);
-
-//background
-// scene.background = spaceTexture;
 
 //render and animate
 function animate() {
   requestAnimationFrame(animate);
 
-  mars.rotation.y += 0.001;
+  mars.rotateOnWorldAxis(axialTilt, 0.0001);
   controls.update();
   renderer.render(scene, camera);
 }
